ef31972dfb3b7cdf46c5b6e769e1a410938c8531
Sung Jun Hong <sung.j.hong93@gmail.com>
2018-10-07 14:51:49 -0700

    Adds tests for URL and name

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,12 +31,23 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
+        it("have URL", function() {
+            for(let i = 0; i < allFeeds.length; i++){
+                expect(allFeeds[i].url).toBeDefined();
+                expect(allFeeds[i].url).not.toBe("");
+            }
+        });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         it("have feed names", function() {
+            for(let i = 0; i < allFeeds.length; i++){
+                expect(allFeeds[i].url).toBeDefined();
+                expect(allFeeds[i].name).not.toBe("");
+            }
+         })
     });
 
 
