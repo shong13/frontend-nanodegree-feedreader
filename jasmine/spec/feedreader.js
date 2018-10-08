39af5a6ab993cfd60d60acf3cd042b9b159caf02
Sung Jun Hong <sung.j.hong93@gmail.com>
2018-10-07 17:00:18 -0700

    adds "New Feed Selection" test suite template

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -96,15 +96,20 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
-        it("", function() {
+        it("ensures entry is in container", function() {
 
         });
     });
     /* TODO: Write a new test suite named "New Feed Selection" */
-    
+    describe("New Feed Selection", function() {
+
+
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
-    
+        it("loads new feed", function() {
+
+        });
+    });
 }());
